4240a3f9c1b0f29c24a6408d50e0096e131f7e92
Daniel Sullivan <Daniel.Sullivan83@Stu.Bmcc.Cuny.Edu>
2026-07-20 10:18:43 -0400

    Let's try this one more time

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const { db } = require("./models");
-const playlistsRouter = require("./routes/playlists")
-const songsRouter = require("./routes/songs")
+const playlistsRouter = require("./routes/playlists");
+const songsRouter = require("./routes/songs");
 const cors = require("cors");
 
-const app = express(); // calling express to create the app
-app.use(express.json()); // use the json middleware to read the JSON request bodies
+const app = express();
+
+app.use(express.json());
 app.use(cors());
+
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
 app.use("/api/playlists", playlistsRouter);
-app.use("/api/songs", songsRouter)
-
-const URL = "https://full-stack-review-front-end.vercel.app/"
+app.use("/api/songs", songsRouter);
 
 db.sync().then(() => {
-  app.listen(URL || 3000, () => {
-    // open port 3000 and see if there are any HTTP requests
-    console.log("Server running on URL"); // console logs when the server is running
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
   });
 });
